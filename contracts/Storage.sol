c01fe814e9878fbb9f8ce58e37f61f6c12374cde
Maksym Hrynenko <mg06062003@gmail.com>
2024-03-23 14:12:13 +0200

    remove payable functionality

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -6,26 +6,13 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IStorage} from "./interfaces/IStorage.sol";
 
 contract Storage is Ownable, IStorage {
-    mapping(string => bool) public messages;
+    mapping(uint256 => bool) public messages;
 
     constructor(address initialOwner) Ownable(initialOwner) {}
 
-    function addMessage(string memory message_) external payable {
+    function addMessage(uint256 message_) public {
         require(!messages[message_], "Storage: such message is already exists");
 
-        require(msg.value >= getCommission(message_), "Storage: comission is too low");
-
         messages[message_] = true;
-    }
-
-    function getCommission(string memory message_) public pure returns (uint256) {
-        uint256 messageLength_ = bytes(message_).length;
-        require(messageLength_ > 0, "Storage: too small message");
-        return messageLength_ * 1 wei;
-    }
-
-    function withdraw(uint256 amount_) external onlyOwner {
-        require(amount_ >= address(this).balance, "Storage: Insufficient contract balance");
-        payable(owner()).transfer(amount_);
     }
 }
--- contracts/interfaces/IStorage.sol
+++ contracts/interfaces/IStorage.sol
@@ -2,7 +2,5 @@
 pragma solidity ^0.8.17;
 
 interface IStorage {
-    // TODO: add proof in the params and verify it here requirment statement inside
-    // the function
-    function addMessage(string memory message_) external payable;
+    function addMessage(uint256 message_) external;
 }
